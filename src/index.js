d0e1cf48231948bb32f02fcdf8358f859028dc8d
Vicky Hoang <vicky.k.hoang@gmail.com>
2023-12-19 14:59:42 -0800

    initial dev env setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "react-dom";
+import App from './components/App';
+//import './src/scss/style.scss'
+
+render(
+  <App />,
+  document.getElementById('root')
+);
